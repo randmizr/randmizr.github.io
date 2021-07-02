0caf0c6014f834a4a0ff8820098f3686c9776ef4
Randmizr <77859666+randmizr@users.noreply.github.com>
2021-07-02 12:38:56 +0300

    randmizr 1.1.0

--- data/outdoorsummer.js
+++ data/outdoorsummer.js
@@ -1,12 +1,12 @@
 export const list =
 [
   {
-    "name": "Go to the beach",
+    "name": "Go to the Beach",
     "description": "Nothing like the sand, shades and a nice sunny day at the beach."
   },
   {
-    "name": "Scubba diving",
-    "description": "The ocean hides the greatest mysteries, maybe you'll come across one today as scuba diving is your activity of choice."
+    "name": "Scubba Diving",
+    "description": "The ocean hides the greatest mysteries, maybe you'll come across one today as scubba diving is your activity of choice."
   },
   {
     "name": "Paragliding",
@@ -14,6 +14,98 @@ export const list =
   },
   {
     "name": "Snorkeling",
-    "description": "It's kinda like scuba diving, but it's not. It's actually snorkeling. Please make sure to tell us what the difference is after your fun trip!"
+    "description": "It's kind of like scubba diving, but it's not. It's actually snorkeling. Please make sure to tell us what the difference is after your fun trip!"
+  },
+  {
+    "name": "Build a Hammock",
+    "description": "Once you're done, we suggest taking a nice long nap on your hammock to compensate the hard work you've been doing."
+  },
+  {
+    "name": "Go Karting",
+    "description": "Drifting at high speed around each turn is the way to go while karting! May the best racer win!"
+  },
+  {
+    "name": "Téléphérique",
+    "description": "An antique attraction leading to the holy grounds of Harissa, the téléphérique is the activity of the day."
+  },
+  {
+    "name": "Go to an Amusement Park",
+    "description": "A good amusement park knows no age, and will make for a great trip!"
+  },
+  {
+    "name": "Set a Lemonade Stand",
+    "description": "Hey you never know, it might be profitable! But if we come by, will you offer us free lemonade?"
+  },
+  {
+    "name": "Bike ride (Manara)",
+    "description": "A bike ride is what we recommend today around the Manara area, plenty of choices to pedal towards."
+  },
+  {
+    "name": "Bike ride (Downtown)",
+    "description": "A bike ride is what we recommend today around the Downtown area, plenty of choices to pedal towards."
+  },
+  {
+    "name": "Take a walk in the park",
+    "description": "Nothing like a walk in the nearest park either on your own to gather some thoughts or with a friend to catch up!"
+  },
+  {
+    "name": "Take a stroll through a flower field",
+    "description": "Get your walking shoes on and start contemplating amidst a field filled with beautiful flowers."
+  },
+  {
+    "name": "Stargazing (Falougha)",
+    "description": "Hope there is no forecast tonight, grab a mat and lay on the grounds of Falougha for some quality stargazing!"
+  },
+  {
+    "name": "Chouwen lake",
+    "description": "It's a 40 minute hike, but you will be rewarded when you reach your destination, don't forget to pack a swimsuit!"
+  },
+  {
+    "name": "Water balloon fight",
+    "description": "Escape the heat of the sun in the backyard with a stack of water balloons ready to be popped!"
+  },
+  {
+    "name": "Find shapes in the clouds",
+    "description": "Clouds hide a lot of images and shapes, do you have a keen eye? You might see the Randmizr logo! No? Too much?"
+  },
+  {
+    "name": "Tyre beach",
+    "description": "You have the possibilities to either take a dip in one of the crystal clear waters of the Tyre shore or just chill at a nearby cafe with a beautiful view of the sunset."
+  },
+  {
+    "name": "Picnic (Tannourine)",
+    "description": "You're in for a lucky one! Today you get the satisfaction of a road trip and a full belly all at once! Tannourine is the beautiful destination to head to."
+  },
+  {
+    "name": "Jazirit el Aranib",
+    "description": "The Palms Island is the oasis where you will be spending a lovely sunny day this afternoon!"
+  },
+  {
+    "name": "Sidon Sea Castle",
+    "description": "What better way to spend a sunny evening than exploring a castle made at sea in Sidon!"
+  },
+  {
+    "name": "Kadisha Grotto (Bsharri)",
+    "description": "One of Bsharri's jewels of nature, the Qadisha Grotto is the prime location you will be cruising up to today."
+  },
+  {
+    "name": "Jet ski Ride",
+    "description": "Take to the waters on a Jet Ski and drive along the coast of your choice."
+  },
+  {
+    "name": "Mountain Biking",
+    "description": "Head to a mountain Bike station and enjoy a ride along a rocky offroad track of your choice."
+  },
+  {
+    "name": "ATV ride",
+    "description": "A dusty offroad ATV Track is on the list for today! Don't forget to take pictures and tag us!"
+  },
+  {
+    "name": "Beach Party",
+    "description": "Either set up or go to a lit Beach party!"
+  },
+  {
+    "name": "Build a Sandcastle",
+    "description": "Do a sandcastle building contest with your friends!"
   }
 ]
